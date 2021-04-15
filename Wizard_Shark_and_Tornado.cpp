082542016e8662b3be4ea555837a80044909d10d
Soyoung <ma2214191@gmail.com>
2021-04-15 23:15:20 +0900

    "1. Tornado 돌리기 완성"

--- Wizard_Shark_and_Tornado.cpp
+++ Wizard_Shark_and_Tornado.cpp
@@ -1,21 +1,20 @@
 // 20057 마법사 상어와 토네이도
 // https://www.acmicpc.net/problem/20057
 
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include <stdio.h>
 #include <queue>
+#include <list>
 using namespace std;
 
-typedef struct _pos {
-	int x;
-	int y;
-	_pos(int _x, int _y) : x(_x), y(_y) {}
-} Pos;
-
-typedef struct _sandInfo {
-	Pos pos;
-	int ratio;
-	_sandInfo(Pos _pos, int _ratio) : pos(_pos), ratio(_ratio) {}
-} SandInfo;
+typedef struct _dir {
+	int dx;
+	int dy;
+	_dir(int _dx, int _dy) : dx(_dx), dy(_dy) {}
+} Dir;
 
 int rotation(int dir) {
 	switch (dir) {
@@ -30,25 +29,79 @@ int rotation(int dir) {
 	}
 }
 
-int N = 0; Pos p_shk;
+int N = 0, out_sand = 0;
 int grid[500][500] = { 0, };
-queue<pair<int, int>> q_tor;
+Dir arr_Dir[4] = { Dir(-1,0),Dir(0,1),Dir(1,0),Dir(0,-1) }; // 위, 오, 아, 왼
+queue<int> q_tor;
+int arr_dx[4][10] = { {1, 1, 0, 0, 1, 1, 0, 0, -2, -1},
+					   {-1, 1, -1, 1, -1, 1, -2, 2, 0, 0},
+					   {-1, -1, 0, 0, 1, 1, 0, 0, 2, 1},
+					   {-1, 1, -1, 1, -1, 1,-2, 2, 0, 0} };
+int arr_dy[4][10] = { {-1, 1, -1, 1, -1, 1, -2, 2, 0, 0},
+					   {-1, -1, 0, 0, 1, 1, 0, 0, 2, 1 },
+					   {-1, 1, -1, 1, -1, 1, -2, 2, 0, 0},
+					   {1, 1, 0, 0, -1, -1, 0, 0, -2, -1} };
+float p[9] = { 0.01, 0.01, 0.07, 0.07, 0.10, 0.10, 0.02, 0.02, 0.05 };
 
 int main() {
-	scanf("%d", &N);`
+	scanf("%d", &N);
 
-	p_shk = Pos((N / 2) + 1, (N / 2) + 1);
-	for (int i = 1; i <= N; i++) {
-		int a = 0;
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	int shk_x = (N - 1) / 2;	int shk_y = (N - 1) / 2;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
 			scanf("%d", &grid[i][j]);
+
+	// 1. tornado 방향
+	// 규칙: (1 move,1 rotation)*2 + (2 move,1 rotation)*2
+	int dir_idx = 3;
+	for (int m = 1; m <= N - 1; m++) {
+		for (int i = 0; i < 2; i++) {
+			for (int cnt = 1; cnt <= m; cnt++)
+				q_tor.push(dir_idx);
+			dir_idx = rotation(dir_idx);
 		}
 	}
-	// 1. tornado 방향을 구현 N*N-1번 움직임
-	// 2. 방향에 따라 비율 계산이 달라진다는 것
+	// last move
+	for (int cnt = 1; cnt <= N - 1; cnt++)
+		q_tor.push(dir_idx);
+
+	// 2. shark가 움직이는 방향에 따라 비율 계산이 달라진다는 것
+	while (!q_tor.empty()) {
+		int idx = q_tor.front();
+		q_tor.pop();
+		int dx = arr_Dir[idx].dx;		int dy = arr_Dir[idx].dy;
+		int n_shk_x = shk_x + dx, n_shk_y = shk_y + dy;
+		printf("%d, %d \n", n_shk_x, n_shk_y);
+		int n_x = 0, n_y = 0, total_sand = grid[n_shk_x][n_shk_y];
+		for (int j = 0; j < 9; j++) {
+			n_x = n_shk_x - arr_dx[idx][j];
+			n_y = n_shk_y - arr_dy[idx][j];
+			if (n_x < 0 || n_x >= N || n_y < 0 || n_y >= N) {
+				out_sand += total_sand * p[idx];
+				grid[n_shk_x][n_shk_y] -= total_sand * p[idx];
+				continue;
+			}
+			grid[n_x][n_y] += total_sand * p[idx];
+			grid[n_shk_x][n_shk_y] -= total_sand * p[idx];
+		}
+		n_x = n_shk_x - arr_dx[idx][9];
+		n_y = n_shk_y - arr_dy[idx][9];
+		if (n_x < 0 || n_x >= N || n_y < 0 || n_y >= N) {
+			out_sand += grid[n_shk_x][n_shk_y];
+		} else {
+			grid[n_x][n_y] += grid[n_shk_x][n_shk_y];
+		}	
+		grid[n_shk_x][n_shk_y] = 0;
+		shk_x = n_shk_x; shk_y = n_shk_y;
+		for (int i = 0; i < N; i++){
+			for (int j = 0; j < N; j++){
+				printf("%d ", grid[i][j]);
+			}
+			printf("\n");
+		}
+	}
+
 	// 출력: 격자의 밖으로 나간 모래의 양
+	printf("%d \n", out_sand);
 	return 0;
 }
