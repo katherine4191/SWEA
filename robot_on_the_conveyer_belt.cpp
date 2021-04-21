28627340e6e364872ca4298c45a55b8f3a605684
Soyoung <ma2214191@gmail.com>
2021-04-21 23:40:16 +0900

    "제일 먼저 놓인 로봇부터 옮길 수 있는 자료구조: deque"

--- robot_on_the_conveyer_belt.cpp
+++ robot_on_the_conveyer_belt.cpp
@@ -1,4 +1,5 @@
 // 20055 컨베이어 벨트 위의 로봇
+// deque로 푸는 것이 핵심인 문제였다
 // https://www.acmicpc.net/problem/20055
 
 #ifdef _MSC_VER
@@ -6,100 +7,92 @@
 #endif
 
 # include <stdio.h>
-# include <cstring>
-# include <queue>
+# include <iostream>
+# include <deque>
 using namespace std;
 
 int n = 0, k = 0, step = 0, cnt = 0;
-int belt[2][50] = {0,};
-int robot[2][50] ={0,};
-queue<pair<int,int>> q_robot;
+deque <int> belt;
+deque<bool> robot;
 
 // 내구도가 감소하는 경우
 // 1. 어떤 칸에 올라가거나
 // 2. 이동하는 경우
 // 내려가는 위치에 로봇이 있는 경우 로봇은 반드시 땅으로 내려가야 한다.
 
-void rotation() {
-	int up_b = belt[0][n-1], down_f = belt[1][0];
-	for (int i = n-1; i > 0; i--)  // n-1 ~ 1까지
-		belt[0][i] = belt[0][i-1];
-	for (int i = 0; i < n-1; i++) // 0 ~ n-2까지
-		belt[1][i] = belt[1][i + 1];
-	
-	belt[0][0] = down_f;
-	belt[1][n-1] = up_b;
+void print() {
+	for (int i = 0; i < n; i++)
+		printf("%d ", belt[i]);
+	printf("\t ");
+	for (int i = 0; i < n; i++)
+		printf("%d ", robot[i]);
+	printf("\n");
+	for (int i = n; i < 2*n; i++)
+		printf("%d ", belt[i]);
+	printf("\t ");
+	for (int i = n; i < 2*n; i++)
+		printf("%d ", robot[i]);
+	printf("\n"); printf("\n");
 }
 
-void print(){
-	for (int i = 0; i < n; i++)
-		printf("%d ", belt[0][i]);
-	printf("\t ");
-	for (int i = 0; i < n; i++)
-		printf("%d ", robot[0][i]);
-	printf("\n");
-	for (int i = 0; i < n; i++)
-		printf("%d ", belt[1][i]);
-	printf("\t ");
-	for (int i = 0; i < n; i++)
-		printf("%d ", robot[1][i]);
-	printf("\n"); printf("\n"); 
+void rotation() {
+	belt.push_front(belt.back());
+	belt.pop_back();
+
+	robot.push_front(robot.back());
+	robot.pop_back();
+}
+
+void move() {
+	for (int i = n-2; i >= 0; i--){
+		if(robot[i] && !robot[i+1] && belt[i+1] >= 1){
+			robot[i] = false;
+			robot[i+1] = true;
+			belt[i + 1]--;
+		}
+	}
+	// robot 땅에 내리기
+	robot[n - 1] = false;
+}
+
+void put_robot() {
+	if(belt[0]>0) {
+		belt[0]--;
+		robot[0]=true;
+	}	
 }
 
 int get_zero_cnt(){
 	int zero = 0;
-	for (int i = 0; i < n; i++){
-		if(belt[0][i]==0 || belt[1][i]==0)
-			zero += 1;
+	for (int i = 0; i < 2*n; i++){
+		if(belt[i]==0)
+			zero++;
 	}
 	return zero;
 }
 
 int main(void){
-	scanf("%d %d", &n, &k);
+	cin >> n >> k;
 
-	for (int i = 0; i < n; i++)
-		scanf("%d", &belt[0][i]);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &belt[1][i]);
-
+	for (int i = 0; i < 2 * n; i++){
+		int val;
+		cin >> val;
+		belt.push_back(val);
+		robot.push_back(false);
+	}
+	print();
 	while(true){
 		step += 1;
-		printf("step: %d\n",step);
 		rotation();
-		
-		if(belt[0][0] != 0 && robot[0][0] != 1){
-			q_robot.push({0,0});
-			belt[0][0] -= 1;
-			robot[0][0] = 1;
-			print();
-			if (get_zero_cnt() == k) {
-				printf("%d\n", step); return 0;
-			}
+		move();
+		put_robot();
+		print();
+		printf("zero: %d\n", get_zero_cnt());
+		if(get_zero_cnt() >= k){
+			cout << step << "\n";
+			return 0;
 		}
-		int q_sz = q_robot.size();
-		while(q_sz--){
-			int rx = q_robot.front().first;
-			int ry = q_robot.front().second;
-			int ny = ry + 1;
-
-			if(ny == n){ // on the ground
-				q_robot.pop();
-				robot[rx][ry] = 0;
-				// belt도 빼야하나
-			}
-			if(robot[rx][ny] == 0 && belt[rx][ny] > 0){ // can move
-				q_robot.pop();
-				robot[rx][ry] = 0;
-				robot[rx][ny] = 1;
-				belt[rx][ny] -= 1;
-				if (get_zero_cnt() == k) {
-					printf("%d\n", step); break;
-				}
-				q_robot.push({rx,ny});
-			}
-			print();
-		}		
 	}
 	return 0;
 }
+
